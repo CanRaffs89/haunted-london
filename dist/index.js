da867e7c25a829e154c5ba5dcec1bb828433e487
Can <24660563+CanRaffs89@users.noreply.github.com>
2019-12-30 21:44:40 +0000

    Started styling hero image; added animation to hamburger icon

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,13 @@
 function menuToggle() {
+    // Toggle first and third lines of hamburger icon when menu open
     const lines = document.querySelectorAll('.hidden')
     lines.forEach(svg => svg.classList.toggle('menu-active'));
 
+    // Toggle visibility of menu links when menu open
     const links = document.querySelectorAll('.nav-link')
     links.forEach(nav => nav.classList.toggle('active'));
 }
 
+// Add toggle function to menu icon
 document.querySelector('.menu-toggle')
     .addEventListener('click', menuToggle);
