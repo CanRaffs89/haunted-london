fd35b974afe8a96289bfa2ba273743d33067833a
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-10-19 17:18:42 +0100

    Added booking modals with Javascript functionality for opening and closing them

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Menu Toggle Function
 function menuToggle() {
     // Toggle first and third lines of hamburger icon when menu open
     const lines = document.querySelectorAll('.hidden')
@@ -12,9 +13,51 @@ function menuToggle() {
 document.querySelector('.menu-toggle')
     .addEventListener('click', menuToggle);
 
+// ---------------------
+
 // Initialise Flatpickr
-flatpickr("#flatpickr", {
-    altInput: true,
-    altFormat: "F j, Y",
-    dateFormat: "Y-m-d",
-});
+// flatpickr("#flatpickr", {
+//     altInput: true,
+//     altFormat: "F j, Y",
+//     dateFormat: "Y-m-d",
+// });
+
+// ---------------------
+
+// Modal Functionality
+
+// Get all the modal buttons
+var btns = document.querySelectorAll("button.modal-button");
+
+// Get all the modals
+var modals = document.querySelectorAll(".modal");
+
+// Get all the closing spans
+var spans = document.getElementsByClassName("close");
+
+// Loop through the modal buttons and display corresponding modal when clicked
+for (var i = 0; i < btns.length; i++) {
+    btns[i].onclick = function(e) {
+        e.preventDefault(); /* Prevent url from being followed */
+        modal = document.querySelector(e.target.getAttribute("href")); /* Select corresponding modal by href... */
+        modal.style.display = "block"; /* ...and set its display to block */
+    }
+}
+
+// Loop through the closing spans and close modal when clicked
+for (var i = 0; i < spans.length; i++) {
+    spans[i].onclick = function() {
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        }
+    }
+}
+
+// Close the modal when user clicks anywhere outside of it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        }
+    }
+}
